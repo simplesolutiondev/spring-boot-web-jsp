5287c81d478a4db888b11df4904fb9e7157f16fb
simplesolutiondev <simplesolution.devteam@gmail.com>
2022-04-11 23:59:51 +0700

    Spring Boot Web with JSP
    Spring Boot Web with JSP

---
+++ src/main/java/dev/simplesolution/controller/HomeController.java
@@ -0,0 +1,15 @@
+package dev.simplesolution.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to Simple Solution home page!");
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Spring Boot Web with JSP - Simple Solution</title>
+    </head>
+    <body>
+        <h3>${message}</h3>
+    </body>
+</html>
